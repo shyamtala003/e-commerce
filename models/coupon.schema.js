2cedd60581238bd92148ffc8e2b4e9cdae54e430
shyamtala003 <shyamtala003@gmail.com>
2022-12-11 15:30:12 +0530

    coupon schema added

---
+++ models/coupon.schema.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const couponSchema = new mongoose.Schema(
+    {
+        code:{
+            type:String,
+            required:[true,"Please provide coupon name"]
+        },
+        discount:{
+            type:Number,
+            default:0
+        },
+        active:{
+            type:Boolean,
+            default:true
+        }
+
+    },
+    {
+        timestamps:true
+    }
+)
+
+export default mongoose.model("Coupon",couponSchema);
