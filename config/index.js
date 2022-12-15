c98bb53c035185532a685a8b747ad48d1501c444
shyamtala003 <shyamtala003@gmail.com>
2022-12-15 16:02:09 +0530

    connexted db with iife

--- config/index.js
+++ config/index.js
@@ -3,7 +3,9 @@ dotenv.config();
 
 const config={
     JWT_SECRET:process.env.JWT_SECRET,
-    JWT_EXPIRY:process.env.JWT_EXPIRY
+    JWT_EXPIRY:process.env.JWT_EXPIRY,
+    MONGODB_URL: process.env.MONGODB_URL,
+    PORT: process.env.PORT
 }
 
 export default config;
---
+++ index.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import app from './app';
+import config from "./config/index"
+
+(async()=>{
+    try {
+        await mongoose.connect(config.MONGODB_URL)
+        console.log("DB CONNECTED");
+
+        app.on('error', (err) => {
+            console.log("ERROR: ", err);
+            throw err;
+        })
+
+        const onListening = () => {
+            console.log(`Listening on ${config.PORT}`);
+        }
+
+        app.listen(config.PORT, onListening)
+
+    } catch (err) {
+        console.log("ERROR ", err);
+        throw err
+    }
+})()
